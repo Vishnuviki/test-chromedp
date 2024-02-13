1e89ed62c5357e3bbdbea52176bf1c57fb060227
Vishnuviki <vishnu.pannipulathvenugopal@sky.uk>
2024-02-13 19:23:28 +0000

    add actual code

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"fmt"
 	"github.com/chromedp/chromedp"
-	// "log"
-	// "time"
+	"log"
+	"time"
 )
 
 func main() {
@@ -25,138 +25,138 @@ func main() {
 	ctx, cancel := chromedp.NewContext(allocatorContext)
 	defer cancel()
 
-	if err := chromedp.Run(ctx, chromedp.Navigate("https://example.com")); err != nil {
-		// Handle error
-		fmt.Println("Error:", err)
+	// if err := chromedp.Run(ctx, chromedp.Navigate("https://example.com")); err != nil {
+	// 	// Handle error
+	// 	fmt.Println("Error:", err)
+	// }
+
+	// fmt.Println("END")
+
+	var html string
+
+	if err := chromedp.Run(ctx,
+		chromedp.Navigate("https://connectivity-wizard-stubbed-showcase.dev.ce.eu-central-1-aws.npottdc.sky/"),
+
+		chromedp.Sleep(time.Millisecond*800),
+
+		chromedp.Click("/html/body/section/div/div/div/div/div[2]/form/div[1]/input"),
+		chromedp.SendKeys("/html/body/section/div/div/div/div/div[2]/form/div[1]/input", "default"),
+
+		chromedp.Sleep(time.Millisecond*800),
+
+		chromedp.Click("/html/body/section/div/div/div/div/div[2]/form/div[2]/input"),
+		chromedp.SendKeys("/html/body/section/div/div/div/div/div[2]/form/div[2]/input", "8080"),
+
+		chromedp.Sleep(time.Millisecond*800),
+
+		chromedp.Click("/html/body/section/div/div/div/div/div[2]/form/div[3]/input"),
+		chromedp.SendKeys("/html/body/section/div/div/div/div/div[2]/form/div[3]/input", "sky.slack.com"),
+
+		chromedp.Sleep(time.Millisecond*800),
+
+		chromedp.Submit("/html/body/section/div/div/div/div/div[2]/form"),
+
+		chromedp.Sleep(time.Millisecond*800),
+
+		// chromedp.Sleep(time.Millisecond*200),
+
+		chromedp.WaitVisible("/html/body/section/div/div/div/div/div[2]/div/h3"),
+
+		chromedp.Sleep(time.Millisecond*800),
+
+		chromedp.InnerHTML("/html/body/section/div/div/div/div/div[2]/div/h3", &html),
+	); err != nil {
+		log.Fatal(err)
 	}
 
-	fmt.Println("END")
+	fmt.Println("No egress Policy case:", html)
 
-	// var html string
+	ctx, cancel = chromedp.NewContext(allocatorContext)
+	defer cancel()
 
-	// if err := chromedp.Run(ctx,
-	// 	chromedp.Navigate("https://connectivity-wizard-stubbed-showcase.dev.ce.eu-central-1-aws.npottdc.sky/"),
+	if err := chromedp.Run(ctx,
+		chromedp.Navigate("https://connectivity-wizard-stubbed-showcase.dev.ce.eu-central-1-aws.npottdc.sky/"),
 
-	// 	chromedp.Sleep(time.Millisecond*800),
+		chromedp.Sleep(time.Millisecond*800),
 
-	// 	chromedp.Click("/html/body/section/div/div/div/div/div[2]/form/div[1]/input"),
-	// 	chromedp.SendKeys("/html/body/section/div/div/div/div/div[2]/form/div[1]/input", "default"),
+		chromedp.Click("/html/body/section/div/div/div/div/div[2]/form/div[1]/input"),
+		chromedp.SendKeys("/html/body/section/div/div/div/div/div[2]/form/div[1]/input", "tenant-insights"),
 
-	// 	chromedp.Sleep(time.Millisecond*800),
+		chromedp.Sleep(time.Millisecond*800),
 
-	// 	chromedp.Click("/html/body/section/div/div/div/div/div[2]/form/div[2]/input"),
-	// 	chromedp.SendKeys("/html/body/section/div/div/div/div/div[2]/form/div[2]/input", "8080"),
+		chromedp.Click("/html/body/section/div/div/div/div/div[2]/form/div[2]/input"),
+		chromedp.SendKeys("/html/body/section/div/div/div/div/div[2]/form/div[2]/input", "8080"),
 
-	// 	chromedp.Sleep(time.Millisecond*800),
+		chromedp.Sleep(time.Millisecond*800),
 
-	// 	chromedp.Click("/html/body/section/div/div/div/div/div[2]/form/div[3]/input"),
-	// 	chromedp.SendKeys("/html/body/section/div/div/div/div/div[2]/form/div[3]/input", "sky.slack.com"),
+		chromedp.Click("/html/body/section/div/div/div/div/div[2]/form/div[3]/input"),
+		chromedp.SendKeys("/html/body/section/div/div/div/div/div[2]/form/div[3]/input", "abc.googleapis.com"),
 
-	// 	chromedp.Sleep(time.Millisecond*800),
+		chromedp.Sleep(time.Millisecond*800),
 
-	// 	chromedp.Submit("/html/body/section/div/div/div/div/div[2]/form"),
+		chromedp.Submit("/html/body/section/div/div/div/div/div[2]/form"),
 
-	// 	chromedp.Sleep(time.Millisecond*800),
+		chromedp.Sleep(time.Millisecond*800),
 
-	// 	// chromedp.Sleep(time.Millisecond*200),
+		// chromedp.Sleep(time.Millisecond*200),
 
-	// 	chromedp.WaitVisible("/html/body/section/div/div/div/div/div[2]/div/h3"),
+		chromedp.WaitVisible("/html/body/section/div/div/div/div/div[2]/form"),
 
-	// 	chromedp.Sleep(time.Millisecond*800),
+		chromedp.Sleep(time.Millisecond*800),
 
-	// 	chromedp.InnerHTML("/html/body/section/div/div/div/div/div[2]/div/h3", &html),
-	// ); err != nil {
-	// 	log.Fatal(err)
-	// }
+		chromedp.InnerHTML("/html/body/section/div/div/div/div/div[2]/form/div[1]/div/h4", &html),
+	); err != nil {
+		log.Fatal(err)
+	}
 
-	// fmt.Println("No egress Policy case:", html)
+	fmt.Println("DNS lookup case:", html)
 
-	// ctx, cancel = chromedp.NewContext(allocatorContext)
-	// defer cancel()
+	ctx, cancel = chromedp.NewContext(allocatorContext)
+	defer cancel()
 
-	// if err := chromedp.Run(ctx,
-	// 	chromedp.Navigate("https://connectivity-wizard-stubbed-showcase.dev.ce.eu-central-1-aws.npottdc.sky/"),
+	if err := chromedp.Run(ctx,
+		chromedp.Navigate("https://connectivity-wizard-stubbed-showcase.dev.ce.eu-central-1-aws.npottdc.sky/"),
 
-	// 	chromedp.Sleep(time.Millisecond*800),
+		chromedp.Sleep(time.Millisecond*800),
 
-	// 	chromedp.Click("/html/body/section/div/div/div/div/div[2]/form/div[1]/input"),
-	// 	chromedp.SendKeys("/html/body/section/div/div/div/div/div[2]/form/div[1]/input", "tenant-insights"),
+		chromedp.Click("/html/body/section/div/div/div/div/div[2]/form/div[1]/input"),
+		chromedp.SendKeys("/html/body/section/div/div/div/div/div[2]/form/div[1]/input", "tenant-insights"),
 
-	// 	chromedp.Sleep(time.Millisecond*800),
+		chromedp.Sleep(time.Millisecond*800),
 
-	// 	chromedp.Click("/html/body/section/div/div/div/div/div[2]/form/div[2]/input"),
-	// 	chromedp.SendKeys("/html/body/section/div/div/div/div/div[2]/form/div[2]/input", "8080"),
+		chromedp.Click("/html/body/section/div/div/div/div/div[2]/form/div[2]/input"),
+		chromedp.SendKeys("/html/body/section/div/div/div/div/div[2]/form/div[2]/input", "8080"),
 
-	// 	chromedp.Sleep(time.Millisecond*800),
+		chromedp.Sleep(time.Millisecond*800),
 
-	// 	chromedp.Click("/html/body/section/div/div/div/div/div[2]/form/div[3]/input"),
-	// 	chromedp.SendKeys("/html/body/section/div/div/div/div/div[2]/form/div[3]/input", "abc.googleapis.com"),
+		chromedp.Click("/html/body/section/div/div/div/div/div[2]/form/div[3]/input"),
+		chromedp.SendKeys("/html/body/section/div/div/div/div/div[2]/form/div[3]/input", "abc.googleapis.com"),
 
-	// 	chromedp.Sleep(time.Millisecond*800),
+		chromedp.Sleep(time.Millisecond*800),
 
-	// 	chromedp.Submit("/html/body/section/div/div/div/div/div[2]/form"),
+		chromedp.Submit("/html/body/section/div/div/div/div/div[2]/form"),
 
-	// 	chromedp.Sleep(time.Millisecond*800),
+		chromedp.Sleep(time.Millisecond*800),
 
-	// 	// chromedp.Sleep(time.Millisecond*200),
+		// chromedp.Sleep(time.Millisecond*200),
 
-	// 	chromedp.WaitVisible("/html/body/section/div/div/div/div/div[2]/form"),
+		chromedp.WaitVisible("/html/body/section/div/div/div/div/div[2]/form"),
 
-	// 	chromedp.Sleep(time.Millisecond*800),
+		chromedp.Sleep(time.Millisecond*800),
 
-	// 	chromedp.InnerHTML("/html/body/section/div/div/div/div/div[2]/form/div[1]/div/h4", &html),
-	// ); err != nil {
-	// 	log.Fatal(err)
-	// }
+		chromedp.Submit("/html/body/section/div/div/div/div/div[2]/form"),
 
-	// fmt.Println("DNS lookup case:", html)
+		chromedp.Sleep(time.Millisecond*800),
 
-	// ctx, cancel = chromedp.NewContext(allocatorContext)
-	// defer cancel()
+		chromedp.WaitVisible("/html/body/section/div/div/div/div/div[2]/div/h3"),
 
-	// if err := chromedp.Run(ctx,
-	// 	chromedp.Navigate("https://connectivity-wizard-stubbed-showcase.dev.ce.eu-central-1-aws.npottdc.sky/"),
+		chromedp.Sleep(time.Millisecond*800),
 
-	// 	chromedp.Sleep(time.Millisecond*800),
+		chromedp.InnerHTML("/html/body/section/div/div/div/div/div[2]/div/h3", &html),
+	); err != nil {
+		log.Fatal(err)
+	}
 
-	// 	chromedp.Click("/html/body/section/div/div/div/div/div[2]/form/div[1]/input"),
-	// 	chromedp.SendKeys("/html/body/section/div/div/div/div/div[2]/form/div[1]/input", "tenant-insights"),
-
-	// 	chromedp.Sleep(time.Millisecond*800),
-
-	// 	chromedp.Click("/html/body/section/div/div/div/div/div[2]/form/div[2]/input"),
-	// 	chromedp.SendKeys("/html/body/section/div/div/div/div/div[2]/form/div[2]/input", "8080"),
-
-	// 	chromedp.Sleep(time.Millisecond*800),
-
-	// 	chromedp.Click("/html/body/section/div/div/div/div/div[2]/form/div[3]/input"),
-	// 	chromedp.SendKeys("/html/body/section/div/div/div/div/div[2]/form/div[3]/input", "abc.googleapis.com"),
-
-	// 	chromedp.Sleep(time.Millisecond*800),
-
-	// 	chromedp.Submit("/html/body/section/div/div/div/div/div[2]/form"),
-
-	// 	chromedp.Sleep(time.Millisecond*800),
-
-	// 	// chromedp.Sleep(time.Millisecond*200),
-
-	// 	chromedp.WaitVisible("/html/body/section/div/div/div/div/div[2]/form"),
-
-	// 	chromedp.Sleep(time.Millisecond*800),
-
-	// 	chromedp.Submit("/html/body/section/div/div/div/div/div[2]/form"),
-
-	// 	chromedp.Sleep(time.Millisecond*800),
-
-	// 	chromedp.WaitVisible("/html/body/section/div/div/div/div/div[2]/div/h3"),
-
-	// 	chromedp.Sleep(time.Millisecond*800),
-
-	// 	chromedp.InnerHTML("/html/body/section/div/div/div/div/div[2]/div/h3", &html),
-	// ); err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// fmt.Println("Policy allow case:", html)
+	fmt.Println("Policy allow case:", html)
 
 }
